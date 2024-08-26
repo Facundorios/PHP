e4c7bed3e4e9704641ab39cba9b74450d8a3f9d5
Facundo Ríos <facundorios2005@gmail.com>
2024-08-26 12:25:04 -0300

    feat: 1ra y 2da actividad

---
+++ actividad1.php
@@ -0,0 +1,23 @@
+<?php
+$num = array(1, 2, 3, 4, 5);
+
+
+$datos = array(
+    "Biromes" => array(
+        "rojo",
+        "bic"
+    ),
+    "Lapices" => array(
+        "negro",
+        "fabrecastel"
+    )
+);
+
+for ($i = 0; $i < count($num); $i++) {
+
+    echo $num[$i] . " </br>";
+}
+
+
+
+?>
